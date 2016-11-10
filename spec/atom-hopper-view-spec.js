14c60f3c7543c302762a72b8f5fef60fd21ba4c9
Danny Buonocore <danBuonocore@gmail.com>
2016-11-10 15:58:21 -0500

    Converted package to CoffeeScript

--- spec/atom-hopper-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AtomHopperView from '../lib/atom-hopper-view';
-
-describe('AtomHopperView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
