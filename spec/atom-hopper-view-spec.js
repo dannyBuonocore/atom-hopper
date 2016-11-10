fcf8355b6ec7ff5b18b0104bc1877d9631eb9322
Danny Buonocore <danBuonocore@gmail.com>
2016-11-10 14:10:38 -0500

    Auto-generated Atom stub-package

---
+++ spec/atom-hopper-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomHopperView from '../lib/atom-hopper-view';
+
+describe('AtomHopperView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
